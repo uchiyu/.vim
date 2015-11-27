24332fb6104b15a3507cb4f6438483b136ca5bf2
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2015-11-27 11:44:05 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # vim setting
-cd ~
+cd ~/
 git clone https://github.com/uchiyu/.vim.git
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-# コマンドライン上で:NeobundleInstall
+# :NeobundleInstall
