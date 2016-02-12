02db6780715e5642616eefa9d53bdd1912620ff8
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-02-12 16:46:09 +0900

    add exception

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ ln -s -f ~/.vim/.vimrc ~/
 dir=~/.vim
 
 if [ -e $dir ]; then
+  git pull origin master
+else
   mkdir -p ~/.vim/bundle
   git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
